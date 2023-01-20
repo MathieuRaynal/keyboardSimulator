0a081d8d39f9a3bea52eeb2c3a9dad3abc41394b
Mathieu Raynal <mathieu.raynal@gmail.com>
2023-01-20 17:23:58 +0100

    version vendredi soir

--- KeyboardSimulator/src/fr/irit/elipse/keyboardsimulator/Keyboard.java
+++ KeyboardSimulator/src/fr/irit/elipse/keyboardsimulator/Keyboard.java
@@ -23,33 +23,7 @@ public class Keyboard extends JComponent{
 	}
 	
 	public void initKeyboard() {
-		loadXMLFile("resources/azerty.xml");
-	}
-	
-	public void init() {
-		layout = new Block("Layout");
-		layout.activate();
-		
-		Block line1 = new Block("B1");
-		Key keyA = new Key("A",10,10,50,50);
-		Key keyB = new Key("B",70,10,50,50);
-		Key keyC = new Key("C",130,10,50,50);
-		line1.addChild(keyA);
-		line1.addChild(keyB);
-		line1.addChild(keyC);
-		line1.createArea();
-		
-		Block line2 = new Block("B2");
-		Key keyD = new Key("D",10,70,50,50);
-		Key keyE = new Key("E",70,70,50,50);
-		Key keyF = new Key("F",130,70,50,50);
-		line2.addChild(keyD);
-		line2.addChild(keyE);
-		line2.addChild(keyF);
-		line2.createArea();
-
-		layout.addChild(line1);
-		layout.addChild(line2);
+		loadXMLFile("resources/alpha.xml");
 	}
 	
 	public void loadXMLFile(String fileName) {
--- KeyboardSimulator/src/fr/irit/elipse/keyboardsimulator/KeyboardSimulator.java
+++ KeyboardSimulator/src/fr/irit/elipse/keyboardsimulator/KeyboardSimulator.java
@@ -8,11 +8,16 @@ import javax.swing.JFrame;
 public class KeyboardSimulator implements Observer{
 	Keyboard keyboard;
 	String mot, saisie;
+	int nbActivationBlock, nbValidationBlock, nbActivationKey, nbValidationKey;
 
 	public KeyboardSimulator(Keyboard kb){
 		mot = "adbecf";
 		saisie = "";
 		keyboard = kb;
+		nbActivationBlock = 0;
+		nbValidationBlock = 0;
+		nbActivationKey = 0;
+		nbValidationKey = 0;
 		keyboard.getKeyboardLayout().addObserver(this);
 		keyboard.validate();
 	}
@@ -33,21 +38,32 @@ public class KeyboardSimulator implements Observer{
 			System.out.println(s+" = "+prefixe+"+"+res);
 			switch(prefixe) {
 				case "[A](B)":
-					if(res.contains(mot.subSequence(0, 1)))
+					nbActivationBlock++;
+					if(res.contains(mot.subSequence(0, 1))) {
 						keyboard.validate();
+					}
 				break;
 				case "[A](K)":
-					if(res.equals(mot.subSequence(0, 1)))
+					nbActivationKey++;
+					if(res.equals(mot.subSequence(0, 1))) {
 						keyboard.validate();
+					}
 				break;
 				case "[V](B)":
+					nbValidationBlock++;
 				break;
 				case "[V](K)":
 					saisie = saisie + res;
 					mot = mot.substring(res.length());
+					nbValidationKey++;
 					System.out.println("*************************** Saisie : "+saisie);
+					System.out.println("Validation Key : "+nbValidationKey);
 					if(mot.length()==0) {
 						System.out.println("Saisie terminee");
+						System.out.println("Activation Block : "+nbActivationBlock);
+						System.out.println("Activation Key : "+nbActivationKey);
+						System.out.println("Validation Block : "+nbValidationBlock);
+						System.out.println("Validation Key : "+nbValidationKey);
 						System.exit(0);
 					}
 					keyboard.getKeyboardLayout().init();
